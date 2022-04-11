0537bd0409385f6d3c322ec5fce06aee8f70cf46
DESKTOP-7NBOJSE\Random <vantri_antonius@yahoo.co.id>
2022-04-11 16:23:39 +0700

    Footer

---
+++ application/views/footer.php
@@ -0,0 +1,51 @@
+<footer>
+    <div class="mt-5 pt-5 pb-5 footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-lg-1">
+                    <img src="<?= base_url('assets/images/wp/itda.png') ?>" class="mx-auto d-block pr-2" alt="" style="width: 80px;">
+                </div>
+                <div class="col-lg-5 col-xs-12" style="padding: 15px;">
+                    <h3>Institut Teknologi Dirgantara Adisutjipto</h3>
+                    <p class="pr-5 text-white">
+                        Jl. Janti, Blok R - Lanud Adisutjipto Yogyakarta
+                        <br>
+                        <i class="fa fa-phone" aria-hidden="true">&nbsp;+62 274 451262 (Hunting) </i> <br>
+                        <i class="fa fa-phone" aria-hidden="true">&nbsp;+62 274 451263 (Telp) </i> <br>
+                        <i class="fa fa-fax" aria-hidden="true">&nbsp;+62274451265 (Fax)</i><br>
+                        <i class="fa fa-envelope-o" aria-hidden="true">&nbsp;admin@itda.ac.id</i>
+                    </p>
+                </div>
+                <div class="col-lg-3 col-xs-12 links">
+                    <h4 class="mt-lg-0 mt-sm-3">Fakultas</h4>
+                    <ul class="m-0 p-0">
+                        <li><a href="#">Fakultas Teknik Dirgantara</a></li>
+                        <ol>- Teknik Dirgantara </ol>
+                        <ol>- Teknik Mesin </ol>
+                        <ol>- D3 Aeronautika </ol>
+                        <li><a href="#">Fakultas Teknik Industri</a></li>
+                        <ol>- Informatika</ol>
+                        <ol>- Teknik Elektro </ol>
+                        <ol>- Teknik Industri </ol>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-xs-12 location">
+                    <h4 class="mt-lg-0 mt-sm-4">Media Sosial</h4>
+                    <ul class="social">
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col copyright text-center">
+                    <p><small class="text-white">
+                            2021&copy; Developer Team All. Right Reserved
+                        </small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
