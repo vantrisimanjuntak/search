ee5b35a38da516e727b7a7964ab43f4ba1be056f
DESKTOP-7NBOJSE\Random <vantri_antonius@yahoo.co.id>
2022-04-16 23:52:29 +0700

    Mengubah route login

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 $route['default_controller'] = 'home/cari';
 $route['home'] = 'home/cari';
+$route['login'] = 'control/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
